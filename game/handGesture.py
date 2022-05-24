199faf9e9add052065459cb4edc7c2ddfb03fdfa
unono915 <unono915@gmail.com>
2022-05-24 20:43:34 +0900

    init

--- game/handGesture.py
+++ game/handGesture.py
@@ -2,7 +2,6 @@ import cv2
 import mediapipe as mp
 import math
 from time import time, sleep
-from pynput.keyboard import Key, Controller
 import numpy as np
 
 gesture = {
@@ -17,7 +16,6 @@ gesture = {
     8: "circle",
 }
 
-keyboard = Controller()
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.8, min_tracking_confidence=0.8)
@@ -29,7 +27,7 @@ label = file[:, -1].astype(np.float32)
 knn = cv2.ml.KNearest_create()
 knn.train(angle, cv2.ml.ROW_SAMPLE, label)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 
 def angle_0to5(a, b):
@@ -45,7 +43,6 @@ def angle_0to5(a, b):
         if dy < 0.0:
             angle += 360.0
     return (angle + 180) % 360
-
 
 def gesture(res):
     joint = np.zeros((21, 3))
@@ -79,25 +76,20 @@ def gesture(res):
 
 
 def cval(queue_cam2game, queue_game2cam):
-
     # ready가 3이되면 슈팅 가능
     gesture_wait = 0
-    shooting_wait = 0
+    wait_1to2 = 0
+    wait_1to3 = 0
+    okay_2or3 = 0
     ready_tf = False
     shootingtime = 0
     shooting_s = 0
     max_dist = 0
     max_power = 0
     cnt = 0
-    init_page = True
     mode_idx = 0
-    al_select = True
-    while cap.isOpened():
-        try:  # handGesture 에서 queue를 이용해 값 가져오기
-            init_page = queue_game2cam.get_nowait()
-        except Exception:
-            pass
 
+    while cap.isOpened():
         cnt += 1
         send = {"shoot_power": 0, "shoot_angle": None, "gesture": 0, "select_Al": None, "ready": False}
 
@@ -117,68 +109,82 @@ def cval(queue_cam2game, queue_game2cam):
                         (hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y),
                     )
                 )
+                d1_to_3 = abs(
+                    math.dist(
+                        (hand_landmarks.landmark[4].x, hand_landmarks.landmark[4].y),
+                        (hand_landmarks.landmark[12].x, hand_landmarks.landmark[12].y),
+                    )
+                )
+
                 d1_to_2 = d1_to_2 * 1000.0
+                d1_to_3 = d1_to_3 * 1000.0
                 idx = gesture(hand_landmarks)
                 if mode_idx != idx:
                     gesture_wait = 0
                     mode_idx = idx
                 if mode_idx == idx and idx != 0:
                     gesture_wait += 1
-                if gesture_wait == 14:
+                if gesture_wait == 20:
                     send["gesture"] = mode_idx
-                    # print("gesture send")
+                    print("gesture send")
                     # queue_cam2game.put(send)
                     # print("Let's game")
-
-                if al_select:
-                    if hand_landmarks.landmark[9].x < 0.3:
-                        send["select_Al"] = 0
-                        al_select = False
-                    elif hand_landmarks.landmark[9].x > 0.7:
-                        send["select_Al"] = 1
-                        al_select = False
-                else:
-                    if 0.3 < hand_landmarks.landmark[9].x < 0.7:
-                        al_select = True
 
                 if cnt % 2 and not ready_tf:
                     shoot_angle = angle_0to5(hand_landmarks.landmark[0], hand_landmarks.landmark[5])
                     send["shoot_angle"] = shoot_angle
                     # print(shoot_angle, "도")
 
-                # 3초동안 Okay손모양하고 있으면 Ready완료
+                # 2초동안 Okay손모양하고 있으면 Ready완료
                 if d1_to_2 < 100 and not ready_tf:
-                    shooting_wait += 1 / 10
+                    wait_1to2 += 1 / 10
                 else:
-                    shooting_wait = 0
-                if shooting_wait > 2:
+                    wait_1to2 = 0
+                if wait_1to2 > 2:
                     start_dist = d1_to_2
                     ready_tf = True
+                    okay_2or3 = 2
+                    print("쏘세요!")
+
+                if d1_to_3 < 100 and not ready_tf:
+                    wait_1to3 += 1 / 10
+                else:
+                    wait_1to3 = 0
+                if wait_1to3 > 2:
+                    start_dist = d1_to_3
+                    ready_tf = True
+                    okay_2or3 = 3
                     print("쏘세요!")
 
                 send["ready"] = ready_tf
                 # print(ready_tf)
 
-                # 2초안에 슈팅
+                # 1초안에 슈팅
                 if ready_tf:
+                    if okay_2or3 == 2:
+                        check_dist = d1_to_2
+                    elif okay_2or3 == 3:
+                        check_dist = d1_to_3
+                    print(check_dist)
                     if shootingtime == 0:
                         shootingtime = time()
-                    if d1_to_2 > 100:
+                    if check_dist > 100:
                         shooting_s += 1
-                    if d1_to_2 > max_dist:
-                        max_dist = d1_to_2
+                    if check_dist > max_dist:
+                        max_dist = check_dist
                         max_shooting_s = shooting_s
                         if max_shooting_s != 0:
-                            max_power = (max_dist - start_dist) / max_shooting_s
-                            # max_power = max_dist - start_dist
+                            # max_power = (max_dist - start_dist) / max_shooting_s
+                            max_power = max_dist - start_dist
 
                     if time() - shootingtime > 1:
-                        send["shoot_power"] = max_power * 10
+                        send["shoot_power"] = max_power * 3
                         ready_tf = False
                         shootingtime = 0
                         shooting_s = 0
                         max_dist = 0
                         max_power = 0
+                        okay_2or3 = 0
 
                 queue_cam2game.put(send)
 
